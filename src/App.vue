b9ec5bf2b8f2546109fbe10bc5f2c8031b01765e
echo-mei <739181006@qq.com>
2018-08-09 15:28:25 +0800

    环境搭建

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <el-button type="primary">Button</el-button>
     <router-view/>
   </div>
 </template>
